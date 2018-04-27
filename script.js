3b48a2a25bf4898dea6df7b53441f469ac1a9b96
Brad <anuarditucalan@gmail.com>
2018-04-27 16:35:22 +0100

    transfer file 1st file

---
+++ script.js
@@ -0,0 +1,12 @@
+
+<script>
+// Initialize tooltip component
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
+
+// Initialize popover component
+$(function () {
+  $('[data-toggle="popover"]').popover()
+})
+</script>
